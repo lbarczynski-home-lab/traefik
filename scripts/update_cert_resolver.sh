338bfbb682a3c331872382a7a20393b76c2d132d
Łukasz Barczyński <barczynski.lukasz@gmail.com>
2026-06-29 16:28:10 +0200

    Update cert resolver also in other files

--- scripts/update_cert_resolver.sh
+++ scripts/update_cert_resolver.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-RESOLVER_NAME=$1
+TARGET_FILE=$1
+RESOLVER_NAME=$2
 
-if [ -z "$RESOLVER_NAME" ]; then
-  echo "Usage: $0 <resolver_name>"
+if [ -z "$TARGET_FILE" ] || [ -z "$RESOLVER_NAME" ]; then
+  echo "Usage: $0 <target_file> <resolver_name>"
   exit 1
 fi
 
-echo "Overriding certResolver to $RESOLVER_NAME..."
-sed -i "s/certResolver: letsencrypt_prod/certResolver: $RESOLVER_NAME/g" src/shared/static-config/traefik.yml
+if [ ! -f "$TARGET_FILE" ]; then
+  echo "Error: Target file '$TARGET_FILE' does not exist."
+  exit 1
+fi
+
+echo "Overriding certResolver in $TARGET_FILE to $RESOLVER_NAME..."
+sed -i "/certResolver/s/letsencrypt_prod/$RESOLVER_NAME/g" "$TARGET_FILE"
